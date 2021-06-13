e8f7aedeed752583ca7d4c16a1d5e9412ae3f8e1
Andrew Montagne <andrew@montagne.uk>
2021-06-13 06:37:48 +0100

    Quoting env strings.

--- mit/tools/env.py
+++ mit/tools/env.py
@@ -3,7 +3,7 @@ import os, datetime, subprocess
 print('// BEGIN_ENV_VARS')
 
 for key in os.environ:
-	print('#define ENV_%s "%s"' % (key.upper(), os.environ[key]))
+	print('#define ENV_%s "%s"' % (key.upper(), os.environ[key].replace('"', '\\"').replace('\n', '\\n')))
 
 print('#define ENV_BUILD_TIME %s' % datetime.datetime.now().isoformat())
 
