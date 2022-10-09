55170bd30ec97dfa357f786c39925dca8bbc9090
Andrew Montagne <andrew@montagne.uk>
2022-10-09 11:08:45 +0100

    fix mapmerge.bat

--- closed-source/maps/mapmerge.bat
+++ closed-source/maps/mapmerge.bat
@@ -1,9 +1,9 @@
 1>2# : ^
 '''
 @echo off
-WHERE python >nul 2>nul
+WHERE python3 >nul 2>nul
 IF %ERRORLEVEL% NEQ 0 ECHO You don't appear to have python installed, go get Python 3.5 or newer! && PAUSE && EXIT /b
-python -x "%~f0" %*
+python3 -x "%~f0" %*
 exit /b
 rem ^
 '''
@@ -12,6 +12,7 @@ import sys
 
 if sys.version_info < (3, 5):
     print("Python 3.5 or newer is required!")
+    input("\nPress Enter to continue...")
     exit(-1)
 
 sys.path.append("../../mit/tools")
